df89b2e51ea2b006fc9a3474e8921ad5b0065967
Wilson Xavier <87943439+VirsoRaivoky@users.noreply.github.com>
2022-09-30 00:31:12 -0300

    Teste

--- demo/src/main/java/strategy/CalcularFP.java
+++ demo/src/main/java/strategy/CalcularFP.java
@@ -1,7 +1,7 @@
 package strategy;
 
 public class CalcularFP implements InterfaceCalculoJogo {
-
+//! Teste
     @Override
     public double calcular(Jogo jogo, Cliente cliente) {
         return cliente.getSaldo() - jogo.getPreco();
