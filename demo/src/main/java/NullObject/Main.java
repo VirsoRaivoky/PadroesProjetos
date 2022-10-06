cc14d8935ed622bc67ae4a50fd661e892c64e5fe
Wilson Xavier <87943439+VirsoRaivoky@users.noreply.github.com>
2022-10-06 19:39:59 -0300

    Update Main.java

--- demo/src/main/java/NullObject/Main.java
+++ demo/src/main/java/NullObject/Main.java
@@ -2,8 +2,19 @@ package NullObject;
 
 public class Main {
 
-  public static void main (String[] args){
-    
+  public static void main(String[] args) {
+
+    JogoAbstrato j1 = Jogo.getJogo("Ultrakill");
+    JogoAbstrato j2 = Jogo.getJogo("God of War");
+    JogoAbstrato j3 = Jogo.getJogo("Terraria");
+
+    System.out.println("Jogos:");
+    System.out.println(j1.getNome());
+    System.out.println(j2.getNome());
+    System.out.println(j3.getNome());
+
+    if (j3 instanceof JogoNull) {
+      System.out.println("Jogo 3 é Nulo!");
+    }
   }
-  
 }
